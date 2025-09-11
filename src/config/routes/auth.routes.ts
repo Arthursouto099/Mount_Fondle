9f8f5c025f7efdc8389049fc773b1218fd06ad97
PEDRO MACHADO DA SILVA <02339335060@senacrs.edu.br>
2025-09-11 17:19:56 -0300

    update

--- src/config/routes/auth.routes.ts
+++ src/config/routes/auth.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { AuthController } from "../../controllers/AuthController";
-import {CreateUserDTO} from '';
-import {LoginUserDTO} from '';
+import {createUserDTO} from '../../dtos/createUserDTOs';
+import {userLogin} from '../../dtos/LoginUSerDTOs';
 import { validateDTO } from "../../middlewares/UserValidateDTO";
 
 
 const router = Router();
 const controller = new AuthController()
 
-router.post('/register',validateDTO(CreateUserDTO),controller.register.bind(controller))
-router.post('/login',validateDTO(LoginUserDTO),controller.login.bind(controller))
+router.post('/register',validateDTO(createUserDTO),controller.register.bind(controller))
+router.post('/login',validateDTO(userLogin),controller.login.bind(controller))
 
 export default router;
